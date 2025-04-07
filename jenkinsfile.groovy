138741afad0bf57ad4b565b79cf2087cbccff329
DrDiomedes <114020818+DrDiomedes@users.noreply.github.com>
2025-04-06 23:03:09 -0500

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -12,7 +12,7 @@ pipeline {
   stages {
     stage('Checkout') {
       steps {
-        git 'https://github.com/DrDiomedes/sonar-qube.git'
+        git branch: 'main', url: 'https://github.com/DrDiomedes/sonar-qube.git'
       }
     }
 
