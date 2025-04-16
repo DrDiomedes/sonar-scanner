a9b4e5eafa3c949dfa7571c74dc412921ffd45a7
DrDiomedes <114020818+DrDiomedes@users.noreply.github.com>
2025-04-16 18:20:03 -0500

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -8,6 +8,7 @@ pipeline {
   environment {
     PROJECT_ROOT = '.'
     SONARQUBE_URL = 'http://sonarqube.sonarqube.svc:9000/sonarqube'
+    PROJECT = 'https://github.com/veracode/verademo.git'
   }
 
   stages {
@@ -25,11 +26,19 @@ pipeline {
       steps {
         withSonarQubeEnv('sonarqube') {
           withCredentials([string(credentialsId: '31aa0c79-c552-4d6c-9c22-fd5e646438ad', variable: 'SONAR_TOKEN')]) {
+          
+          def repoUrl = PROJECT
+          def appname = repoUrl.tokenize('/').last().replace('.git', '')  
+          echo "Aplicación identificada: ${appname}"
+          def buildid = env.BUILD_ID
+          def commitcode = env.GIT_COMMIT
+          def timestamp = new Date().format("yyyyMMdd-HHmm")
+          def scanversion = "${appname}-${commitcode}-${timestamp}"  
             sh """
               ${scannerHome}/bin/sonar-scanner \
-                -Dsonar.projectKey=prueba-pipeline \
-                -Dsonar.projectName=SonarPipeline \
-                -Dsonar.projectVersion=1.0 \
+                -Dsonar.projectKey=${appname} \
+                -Dsonar.projectName=${appname} \
+                -Dsonar.projectVersion=${scanversion} \
                 -Dsonar.exclusions=**/*.java \
                 -Dsonar.sources=${PROJECT_ROOT} \
                 -Dsonar.token=${SONAR_TOKEN} \
