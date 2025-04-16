6bb536bb75d73116ed4b1cbb12d69344d56f7165
DrDiomedes <114020818+DrDiomedes@users.noreply.github.com>
2025-04-16 18:28:54 -0500

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -24,34 +24,34 @@ pipeline {
         scannerHome = tool 'sonar-scanner'
       }
       steps {
-        withSonarQubeEnv('sonarqube') {
-          withCredentials([string(credentialsId: '31aa0c79-c552-4d6c-9c22-fd5e646438ad', variable: 'SONAR_TOKEN')]) {
-          
-          def repoUrl = PROJECT
-          def appname = repoUrl.tokenize('/').last().replace('.git', '')  
-          echo "Aplicación identificada: ${appname}"
-          def buildid = env.BUILD_ID
-          def commitcode = env.GIT_COMMIT
-          def timestamp = new Date().format("yyyyMMdd-HHmm")
-          def scanversion = "${appname}-${commitcode}-${timestamp}"  
-            sh """
-              ${scannerHome}/bin/sonar-scanner \
-                -Dsonar.projectKey=${appname} \
-                -Dsonar.projectName=${appname} \
-                -Dsonar.projectVersion=${scanversion} \
-                -Dsonar.exclusions=**/*.java \
-                -Dsonar.sources=${PROJECT_ROOT} \
-                -Dsonar.token=${SONAR_TOKEN} \
-                -Dsonar.host.url=${SONARQUBE_URL}
-            """
-            sh """
-            curl -i http://sonarqube.sonarqube.svc:9000/sonarqube
-            """
+        script{  
+          withSonarQubeEnv('sonarqube') {
+            withCredentials([string(credentialsId: '31aa0c79-c552-4d6c-9c22-fd5e646438ad', variable: 'SONAR_TOKEN')]) {
+            def repoUrl = PROJECT
+            def appname = repoUrl.tokenize('/').last().replace('.git', '')  
+            echo "Aplicación identificada: ${appname}"
+            def buildid = env.BUILD_ID
+            def commitcode = env.GIT_COMMIT
+            def timestamp = new Date().format("yyyyMMdd-HHmm")
+            def scanversion = "${appname}-${commitcode}-${timestamp}"  
+              sh """
+                ${scannerHome}/bin/sonar-scanner \
+                  -Dsonar.projectKey=${appname} \
+                  -Dsonar.projectName=${appname} \
+                  -Dsonar.projectVersion=${scanversion} \
+                  -Dsonar.exclusions=**/*.java \
+                  -Dsonar.sources=${PROJECT_ROOT} \
+                  -Dsonar.token=${SONAR_TOKEN} \
+                  -Dsonar.host.url=${SONARQUBE_URL}
+              """
+              sh """
+              curl -i http://sonarqube.sonarqube.svc:9000/sonarqube
+              """
+            }
           }
         }
       }
     }
-
     stage('Verificar resultado del análisis') {
       steps {
         timeout(time: 2, unit: 'MINUTES') {
