d2cb98e79ca8a8fdc4acdeba628a380ff0030e04
DrDiomedes <114020818+DrDiomedes@users.noreply.github.com>
2025-04-16 22:33:54 -0500

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,62 +1,23 @@
+@Library('devsecops_library') _
+
 pipeline {
   agent any
-
-  tools {
-    nodejs 'nodejs'
-  }
-
   environment {
+    PROJECT = 'https://github.com/veracode/verademo.git'
     PROJECT_ROOT = '.'
     SONARQUBE_URL = 'http://sonarqube.sonarqube.svc:9000/sonarqube'
-    PROJECT = 'https://github.com/veracode/verademo.git'
   }
 
   stages {
     stage('Checkout') {
       steps {
-        /// git branch: 'main', url: 'https://github.com/DrDiomedes/sonar-qube.git'
-        git branch: 'main', url: 'https://github.com/veracode/verademo.git'
+        git branch: 'main', url: "${PROJECT}"
       }
     }
 
-    stage('Ejecutar escaneo SonarQube') {
-      environment {
-        scannerHome = tool 'sonar-scanner'
-      }
+    stage('Análisis SonarQube') {
       steps {
-        script{  
-          withSonarQubeEnv('sonarqube') {
-            withCredentials([string(credentialsId: '31aa0c79-c552-4d6c-9c22-fd5e646438ad', variable: 'SONAR_TOKEN')]) {
-            def repoUrl = PROJECT
-            def appname = repoUrl.tokenize('/').last().replace('.git', '')  
-            echo "Aplicación identificada: ${appname}"
-            def buildid = env.BUILD_ID
-            def commitcode = env.GIT_COMMIT
-            def timestamp = new Date().format("yyyyMMdd-HHmm")
-            def scanversion = "${appname}-${commitcode}-${timestamp}"  
-              sh """
-                ${scannerHome}/bin/sonar-scanner \
-                  -Dsonar.projectKey=${appname} \
-                  -Dsonar.projectName=${appname} \
-                  -Dsonar.projectVersion=${scanversion} \
-                  -Dsonar.exclusions=**/*.java \
-                  -Dsonar.sources=${PROJECT_ROOT} \
-                  -Dsonar.token=${SONAR_TOKEN} \
-                  -Dsonar.host.url=${SONARQUBE_URL}
-              """
-              sh """
-              curl -i http://sonarqube.sonarqube.svc:9000/sonarqube
-              """
-            }
-          }
-        }
-      }
-    }
-    stage('Verificar resultado del análisis') {
-      steps {
-        timeout(time: 2, unit: 'MINUTES') {
-          waitForQualityGate abortPipeline: true
-        }
+        sonarScan()
       }
     }
   }
