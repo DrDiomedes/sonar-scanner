a775638c56fd0d647f1854debdef8f9d0b07df29
DrDiomedes <114020818+DrDiomedes@users.noreply.github.com>
2025-04-23 10:15:00 -0500

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -28,6 +28,7 @@ pipeline {
           semgrep scan ${PROJECT_ROOT} \
             --config auto \
             --timeout-threshold 10000 \
+            --metrics=off \
             --json -output semgrep-result.json
         '''
       }
