ccb5a5e5df9c9a6eb808ed7c2fff87b7662f2605
DrDiomedes <114020818+DrDiomedes@users.noreply.github.com>
2025-04-23 12:50:52 -0500

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -42,7 +42,7 @@ pipeline {
             --config semgrep-rules/owasp-top-ten.yml \
             --metrics=off \
             --timeout-threshold 10000 \
-            --json-output semgrep-result.json
+            
             --verbose
         '''
       }
