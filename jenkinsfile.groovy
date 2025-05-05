9af63b34f8ab17d55575ebd2c36b1509072af8ce
DrDiomedes <114020818+DrDiomedes@users.noreply.github.com>
2025-05-05 13:05:19 -0500

    Update jenkinsfile.groovy

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -31,18 +31,18 @@ pipeline {
     stage('registro DefectDojo') {
       steps {
         sh """
-        file=$(ls sast-*.json | head -n 1)
-        echo "Archivo detectado: $file"
-    
-        curl -X POST "curl http://defectdojo-django.defectdojo.svc/api/v2/import-scan/" \
-          -H "Authorization: Token 5a79a17492584808dc2407325923269a6d3df3b6" \
-          -F 'scan_type=Semgrep JSON Report' \
-          -F 'engagement=Semgrep Scan' \
-          -F 'file=@sast-verademo-<algo>.json' \
-          -F 'active=true' \
-          -F 'verified=true' \
-          -F 'scan_date='$(date +%Y-%m-%d) \
-          -F 'minimum_severity=Low'
+          file=$(ls sast-*.json | head -n 1)
+          echo "Archivo detectado: $file"
+      
+          curl -X POST "curl http://defectdojo-django.defectdojo.svc/api/v2/import-scan/" \
+            -H "Authorization: Token 5a79a17492584808dc2407325923269a6d3df3b6" \
+            -F 'scan_type=Semgrep JSON Report' \
+            -F 'engagement=Semgrep Scan' \
+            -F 'file=@sast-verademo-<algo>.json' \
+            -F 'active=true' \
+            -F 'verified=true' \
+            -F 'scan_date='$(date +%Y-%m-%d) \
+            -F 'minimum_severity=Low'
         
         """
         
